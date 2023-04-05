7265113b9cf139399d1d5036c4a90d3b90fdb863
WouterrG <wouterdegruijl@quicknet.nl>
2023-04-05 14:59:15 +0200

    implemented Delete modal

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -1,71 +1,63 @@
 <template>
-    
-    <div
-        class="card mb-4"
-        >
-            <div class="card-content">
-                <div class="content">
-                    {{ note.content }}
-                    <div class="has-text-right has-text-grey-light mt-2">
-                        <small>{{ characterLength }}</small>
-                    </div>
-                </div>
-            </div>
-            <footer class="card-footer">
-                <RouterLink
-                  :to="`/editNote/${ note.id }`"
-                  class="card-footer-item"
-                  href="#"
-                >
-                    Edit
-                </RouterLink>
-                <a
-                @click.prevent="storeNotes.deleteNote(note.id)"
-                  class="card-footer-item"
-                  href="#"
-                >
-                    Delete
-                </a>
-            </footer>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+        <div class="has-text-right has-text-grey-light mt-2">
+          <small>{{ characterLength }}</small>
         </div>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <RouterLink
+        :to="`/editNote/${note.id}`"
+        class="card-footer-item"
+        href="#"
+      >
+        Edit
+      </RouterLink>
+      <a
+        @click.prevent="modals.deleteNote = true"
+        class="card-footer-item"
+        href="#"
+      >
+        Delete
+      </a>
+    </footer>
+    <ModalDeleteNote
+      v-if="modals.deleteNote"
+      v-model="modals.deleteNote"
+      :noteId="note.id"
+    />
+  </div>
 </template>
 
 <script setup>
-
 // IMPORTS
 
-import { computed, reactive } from 'vue';
-import { useStoreNotes } from '@/stores/storeNotes'
-
-
-// STORE
-
-const storeNotes = useStoreNotes()
-
+import { computed, reactive } from "vue";
+import ModalDeleteNote from "@/components/Notes/ModalDeleteNote.vue";
 
 // PROPS
 
 const props = defineProps({
-    note: {
-        type: Object,
-        required: true
-    }
-})
+  note: {
+    type: Object,
+    required: true,
+  },
+});
 
 // CHARACTER COUNT
 
 const characterLength = computed(() => {
-    let length = props.note.content.length
-    let description =  length > 1 ? 'characters' : 'character'
-    return `${ length } ${ description }`
-})
+  let length = props.note.content.length;
+  let description = length > 1 ? "characters" : "character";
+  return `${length} ${description}`;
+});
 
 // MODALS
 
 const modals = reactive({
-    deleteNote: false,
-    editNote: false
-})
-
-
+  deleteNote: false,
+});
 </script>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,62 +1,54 @@
 <template>
-    <div class="notes">
-
-        <AddEditNote
-            v-model="newNote"
-            placeholderText="Add a new note..."
-            ref="addEditNoteRef"
+  <div class="notes">
+    <AddEditNote
+      v-model="newNote"
+      placeholderText="Add a new note..."
+      ref="addEditNoteRef"
+    >
+      <template #buttons>
+        <button
+          @click="addNote"
+          :disabled="!newNote"
+          class="button is-link has-background-success"
         >
-            <template #buttons>
-                <button
-                      @click="addNote"
-                      :disabled="!newNote"
-                      class="button is-link has-background-success"
-                    >
-                        Add New Note
-                    </button>
-            </template>
-        </AddEditNote>
+          Add New Note
+        </button>
+      </template>
+    </AddEditNote>
 
-        <Note 
-            v-for="note in storeNotes.notes"
-            :key="note.id"  
-            :note="note"
-        />
-
-    </div>
+    <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
+  </div>
 </template>
 
 <script setup>
-
 // IMPORTS
 
-import { ref } from 'vue';
-import Note from '@/components/Notes/Note.vue'
-import AddEditNote from '@/components/Notes/AddEditNote.vue'
-import { useStoreNotes } from '@/stores/storeNotes'
-import { useWatchCharacters } from '@/use/useWatchCharacters'
+import { ref } from "vue";
+import Note from "@/components/Notes/Note.vue";
+import AddEditNote from "@/components/Notes/AddEditNote.vue";
+import { useStoreNotes } from "@/stores/storeNotes";
+import { useWatchCharacters } from "@/use/useWatchCharacters";
 
 // STORE
 
-const storeNotes = useStoreNotes()
+const storeNotes = useStoreNotes();
 
-const addEditNoteRef = ref(null)
+const addEditNoteRef = ref(null);
 
 // DATA
 
-const newNote = ref('')
+const newNote = ref("");
 
 // METHODS
 
 const addNote = () => {
-    storeNotes.addNote(newNote.value)
+  storeNotes.addNote(newNote.value);
 
-    newNote.value = ''
-    addEditNoteRef.value.focusTextarea()
-}
+  newNote.value = "";
+  addEditNoteRef.value.focusTextarea();
+};
 
 // WATCHER
 
-useWatchCharacters(newNote, 75)
-
+useWatchCharacters(newNote, 75);
 </script>
