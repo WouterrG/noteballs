97106d0e1efee4a7aef0219edfd7d03de9dd3f70
WouterrG <wouterdegruijl@quicknet.nl>
2023-04-06 11:23:15 +0200

    Prettier Note

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -1,71 +1,63 @@
 <template>
-    
-    <div
-        class="card mb-4"
-        >
-            <div class="card-content">
-                <div class="content">
-                    {{ note.content }}
-                    <div class="has-text-right has-text-grey-light mt-2">
-                        <small>{{ characterLength }}</small>
-                    </div>
-                </div>
-            </div>
-            <footer class="card-footer">
-                <RouterLink
-                  :to="`/editNote/${ note.id }`"
-                  class="card-footer-item"
-                  href="#"
-                >
-                    Edit
-                </RouterLink>
-                <a
-                @click.prevent="storeNotes.deleteNote(note.id)"
-                  class="card-footer-item"
-                  href="#"
-                >
-                    Delete
-                </a>
-            </footer>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+        <div class="has-text-right has-text-grey-light mt-2">
+          <small>{{ characterLength }}</small>
         </div>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <RouterLink
+        :to="`/editNote/${note.id}`"
+        class="card-footer-item"
+        href="#"
+      >
+        Edit
+      </RouterLink>
+      <a
+        @click.prevent="storeNotes.deleteNote(note.id)"
+        class="card-footer-item"
+        href="#"
+      >
+        Delete
+      </a>
+    </footer>
+  </div>
 </template>
 
 <script setup>
-
 // IMPORTS
 
-import { computed, reactive } from 'vue';
-import { useStoreNotes } from '@/stores/storeNotes'
-
+import { computed, reactive } from "vue";
+import { useStoreNotes } from "@/stores/storeNotes";
 
 // STORE
 
-const storeNotes = useStoreNotes()
-
+const storeNotes = useStoreNotes();
 
 // PROPS
 
 const props = defineProps({
-    note: {
-        type: Object,
-        required: true
-    }
-})
+  note: {
+    type: Object,
+    required: true,
+  },
+});
 
 // CHARACTER COUNT
 
 const characterLength = computed(() => {
-    let length = props.note.content.length
-    let description =  length > 1 ? 'characters' : 'character'
-    return `${ length } ${ description }`
-})
+  let length = props.note.content.length;
+  let description = length > 1 ? "characters" : "character";
+  return `${length} ${description}`;
+});
 
 // MODALS
 
 const modals = reactive({
-    deleteNote: false,
-    editNote: false
-})
-
-
+  deleteNote: false,
+  editNote: false,
+});
 </script>
